94d7e3ed28dcd9f565c9ccf40cef53ef1ead2515
Rick Owens <rick@owenssoftware.com>
2017-01-09 11:24:37 -0600

    no-op - whitespace

--- src/Canteven/HTTP.hs
+++ src/Canteven/HTTP.hs
@@ -35,47 +35,35 @@ import Network.Wai (Middleware, responseStatus, requestMethod,
   rawPathInfo, rawQueryString, Response, ResponseReceived, responseLBS)
 
 
-{- |
-  The class of things that can be read as http message entities.
--}
+{- | The class of things that can be read as http message entities. -}
 class FromEntity e where
-  {- |
-    Decode the entity, according to the specified content type.
-  -}
+  {- | Decode the entity, according to the specified content type. -}
   decodeEntity :: Maybe ContentType -> ByteString -> DecodeResult e
 
 
-{- |
-  The class of things that can be used to generate http message entities.
--}
+{- | The class of things that can be used to generate http message entities. -}
 class ToEntity e where
-  {- |
-    The content type of the respone entity.
-  -}
+  {- | The content type of the respone entity. -}
   getContentType :: e -> ContentType
 
-  {- |
-    The bytes associated with the response entity.
-  -}
+  {- | The bytes associated with the response entity. -}
   getBytes :: e -> ByteString
 
 
-{- |
-  The result of trying to decode a request entity.
--}
+{- | The result of trying to decode a request entity. -}
 data DecodeResult e
   = Unsupported
-    -- ^ Signifies an unsupported content type.
+    {- ^ Signifies an unsupported content type.  -}
   | BadEntity String
-    -- ^ Signifies that the request entity is invalid, and provides some
-    --   kind of reason why.
+    {- ^
+      Signifies that the request entity is invalid, and provides some
+      kind of reason why.
+    -}
   | Ok e
-    -- ^ Successfully decoded the entity.
+    {- ^ Successfully decoded the entity. -}
 
 
-{- |
-  ContentType is an alias for ByteString
--}
+{- | ContentType is an alias for ByteString. -}
 type ContentType = ByteString
 
 
