e08305acb1262fba2ff2e51b5c9f97cd51516e89
Michael Ho <mho@sumall.com>
2017-04-25 11:12:09 -0700

    Move staticSite to Internal module

--- src/Canteven/HTTP.hs
+++ src/Canteven/HTTP.hs
@@ -16,17 +16,16 @@ module Canteven.HTTP (
   staticSite,
 ) where
 
-
+import Canteven.Internal (staticSite)
 import Canteven.Log.MonadLog (LoggerTImpl)
 import Control.Concurrent (threadDelay)
 import Control.Exception (SomeException)
-import Control.Monad (void, join)
+import Control.Monad (void)
 import Control.Monad.Catch (try, throwM)
 import Control.Monad.Logger (runLoggingT, LoggingT, logInfo, logError)
 import Control.Monad.Trans.Class (lift)
 import Data.ByteString (ByteString)
-import Data.List ((\\), find)
-import Data.Maybe (catMaybes)
+import Data.List (find)
 import Data.Monoid ((<>))
 import Data.Text (Text, pack, unpack)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
@@ -34,22 +33,12 @@ import Data.Time.Clock (getCurrentTime, UTCTime, diffUTCTime)
 import Data.UUID (UUID)
 import Data.UUID.V1 (nextUUID)
 import Data.Version (showVersion, Version)
-import Language.Haskell.TH (TExp, Q, runIO)
-import Network.HTTP.Types (internalServerError500, Status, statusCode,
-  statusMessage, ok200)
-import Network.Mime (defaultMimeLookup)
-import Network.Wai (Middleware, responseStatus, requestMethod,
-  rawPathInfo, rawQueryString, Response, ResponseReceived, requestHeaders, responseLBS,
-  modifyResponse, pathInfo)
+import Network.HTTP.Types (internalServerError500, Status, statusCode, statusMessage)
+import Network.Wai (Middleware, responseStatus, requestMethod, rawPathInfo,
+  rawQueryString, Response, ResponseReceived, requestHeaders, responseLBS, modifyResponse)
 import Network.Wai.Middleware.AddHeaders (addHeaders)
 import Network.Wai.Middleware.StripHeaders (stripHeader)
-import System.Directory (getDirectoryContents)
-import System.FilePath.Posix (combine, (</>))
-import System.Posix.Files (isRegularFile, isDirectory, getFileStatus)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Lazy.Char8 as BSL8
 import qualified Data.Text as T
 
 
@@ -208,85 +197,3 @@ setServer serviceName version = addServerHeader . stripServerHeader
 
     {- | The value of the @Server:@ header. -}
     serverValue = encodeUtf8 (serviceName <> "/" <> pack (showVersion version))
-
-
-{- |
-  The Template-Haskell splice @$$(staticSite dir)@ will build a
-  'Middleware' that serves a set of static files determined at
-  compile time, or else passes the request to the underlying
-  'Network.Wai.Application'.
-
-  All files under @dir@ will be served relative to the root path of
-  your web server, so the file @\<dir\>\/foo\/bar.html@ will be served at
-  @http://your-web-site.com/foo/bar.html@
-
-  @since 0.1.3
--}
-staticSite :: FilePath -> Q (TExp Middleware)
-staticSite baseDir = join . runIO $ do
-    files <- readStaticFiles
-    mapM_ (printResource . fst) files
-    return $ [||
-        let
-          {- |
-            Build a middleware that serves a single static file path, or
-            delegates to the underlying application.
-          -}
-          static :: (FilePath, String) -> Middleware
-          static (filename, content) app req respond =
-            let
-              {- | Guess the content type of the static file. -}
-              contentType :: BS.ByteString
-              contentType =
-                defaultMimeLookup
-                . pack
-                $ filename
-            in
-              if pathInfo req == T.split (== '/') (pack filename)
-                then
-                  respond (
-                      responseLBS
-                        ok200
-                        [("content-type", contentType)]
-                        (BSL8.pack content)
-                    )
-                else app req respond
-        in
-          foldr (.) id (fmap static files) :: Middleware
-      ||]
-  where
-    printResource :: String -> IO ()
-    printResource file =
-      putStrLn ("Generating static resource for: " ++ show file)
-
-    {- | Reads the static files that make up the admin user interface. -}
-    readStaticFiles :: IO [(FilePath, String)]
-    readStaticFiles =
-      let
-        findAll :: FilePath -> IO [FilePath]
-        findAll dir = do
-            contents <-
-              (\\ [".", ".."]) <$> getDirectoryContents (baseDir </> dir)
-            dirs <- catMaybes <$> mapM justDir contents
-            files <- catMaybes <$> mapM justFile contents
-            more <- concat <$> mapM (findAll . combine dir) dirs
-            return $ (combine dir <$> files) ++ more
-          where
-            justFile :: FilePath -> IO (Maybe FilePath)
-            justFile filename = do
-              isfile <-
-                isRegularFile <$>
-                  getFileStatus (baseDir </> dir </> filename)
-              return $ if isfile then Just filename else Nothing
-
-            justDir :: FilePath -> IO (Maybe FilePath)
-            justDir filename = do
-              isdir <-
-                isDirectory <$>
-                  getFileStatus (baseDir </> dir </> filename)
-              return $ if isdir then Just filename else Nothing
-      in do
-        allFiles <- findAll "."
-        allContent <- mapM (fmap BS8.unpack . BS.readFile . combine baseDir) allFiles
-        return (zip (drop 2 <$> allFiles) allContent)
-
